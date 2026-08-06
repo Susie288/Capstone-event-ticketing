858595ca9ef45249fe76e84b42370f7ae98c153f
Susie <seyhram@gmail.com>
2026-08-06 08:18:26 +0000

    Update on frontend code

--- frontend-event-ticketing/next.config.mjs
+++ frontend-event-ticketing/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }],
-  },
+  output: "export",
 };
 
 export default nextConfig;
